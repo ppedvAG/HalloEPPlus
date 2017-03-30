5b65b31757592f0b0d4eebcfa6724b3e17fecc07
Andre <AndreRRR@outlook.de>
2017-03-30 14:21:28 +0200

    Beispielprojekt wie Daten in eine Exceldate geschrieben werden können

---
+++ ppedv.HalloEPPlus/ppedv.HalloEPPlus.DatenSchreiben/Program.cs
@@ -0,0 +1,45 @@
+﻿using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace ppedv.HalloEPPlus.DatenSchreiben
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Exceldatei als FileInfo angeben
+            string dateiname = "HalloExcel.xlsx";
+            var fi = new FileInfo(dateiname);
+
+            //wenn Datei existiert, dann löschen
+            if (fi.Exists)
+                fi.Delete();
+
+            //dem ExcelPackage die FileInfo mit unserer Datei übergeben
+            //wird bei speichern neu angelegt oder falls existiert zum lesen geöffnet
+            using (var pack = new ExcelPackage(fi))
+            {
+                //neues WorkSheet erstellen mit den Titel "Hallo"
+                var ws = pack.Workbook.Worksheets.Add("Hallo");
+
+                ws.Cells["A1"].Value = "Hallo A1";
+
+                //Änderungen der Datei abspeichern
+                pack.Save();
+            }
+
+            //Datei mit dem Standardprogramm für .xlsx Dateien starten
+            Process.Start(dateiname);
+
+
+            Console.WriteLine("Ende");
+            Console.ReadKey();
+        }
+    }
+}
